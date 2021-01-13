8962b1a95d8677b48f11fd98442a65d344350787
Yichao Li <yichao.li@rampgroup.com>
2021-01-13 01:34:51 -0500

    first commit

---
+++ pages/index/index.js
@@ -0,0 +1,39 @@
+Page({
+  data: {
+    background: ['swiper-item-text-1', 'swiper-item-text-2', 'swiper-item-text-3'],
+    indicatorDots: true,
+    vertical: false,
+    autoplay: false,
+    circular: false,
+    interval: 2000,
+    duration: 500,
+    previousMargin: 50,
+    nextMargin: 50
+  },
+  changeProperty: function (e) {
+    var propertyName = e.currentTarget.dataset.propertyName
+    var newData = {}
+    newData[propertyName] = e.detail.value
+    this.setData(newData)
+  },
+  changeIndicatorDots: function (e) {
+    this.setData({
+      indicatorDots: !this.data.indicatorDots
+    })
+  },
+  changeAutoplay: function (e) {
+    this.setData({
+      autoplay: !this.data.autoplay
+    })
+  },
+  intervalChange: function (e) {
+    this.setData({
+      interval: e.detail.value
+    })
+  },
+  durationChange: function (e) {
+    this.setData({
+      duration: e.detail.value
+    })
+  }
+})
